fefb87d85c389bf454a777e18a48168d9db74119
Nick Palenchar <nickpalenchar@users.noreply.github.com>
2018-09-10 12:54:21 -0400

    Update contents.sh

--- _build/contents.sh
+++ _build/contents.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# Automatically generates a table of contents at the top of a md file. It overwrites old contents used with this script
+# 
+# USAGE
+# contests.sh file
+#
+# file - the name of the file to generate the contents
+#
+# if -f is given, the file is overwritten and there is no extra output needed.
+
+# remove old contents
+cat $1 | sed '/^## Contents/,/\-\-\-/d'
+
+# read and update contents
+# TODO
